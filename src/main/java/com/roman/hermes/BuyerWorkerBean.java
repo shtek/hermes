0eba150d1c6608fe4188b5ae910f3d3d8976c88d
Roman Shtekelman <shtek@yahoo.com>
2021-05-05 18:19:41 +0100

    first navigation

--- src/main/java/com/roman/hermes/BuyerWorkerBean.java
+++ src/main/java/com/roman/hermes/BuyerWorkerBean.java
@@ -1,11 +1,16 @@
 package com.roman.hermes;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BuyerWorkerBean {
@@ -16,23 +21,45 @@ public class BuyerWorkerBean {
 
      try{
          driver.get(url);
-       //  WebDriverWait wait = new WebDriverWait(driver, 100);
+        WebDriverWait wait = new WebDriverWait(driver, 30);
 
-         WebElement account = driver.findElement(By.className("button.button-icon.button-not-black"));
-         Thread.sleep(5000);
-        // WebElement account  = driver.findElementByClassName("button.button-icon.button-not-black");
-        //account.click();
-         //By by = new By.ByClassName("button.button-icon.button-not-black");
-      //   WebElement username=  driver.findElement(By.id("login-email"));
+        // WebElement account = driver.findElement(By.className("button.button-icon.button-not-black"));
+        // WebElement search = driver.findElement(By.name("search"));
+       //String  aTitle = driver.getPageSource();
 
-        // WebElement password=driver.findElement(By.id("login-password"));
-        // WebElement login=driver.findElement(By.className("button.form-submit.ripple-effect"));
-
-         //username.sendKeys("username");
-        // password.sendKeys("password");
-       //  password.submit();
         // Thread.sleep(10000);
-         System.out.println("bzz");
+       List<WebElement > accounts  = driver.findElementsByXPath("//*[contains(@class,'button button-icon button-not-black')]");
+      accounts.get(1).click();
+       Thread.sleep(2000);
+
+         WebElement username=  driver.findElement(By.id("login-email"));
+
+          WebElement password=driver.findElement(By.id("login-password"));
+    //     String inputText = "shtek@yahoo.com";
+      //   String pass = "1973roman";
+       //  String js = "arguments[0].setAttribute('value','"+inputText+"')" ;
+       //              ((JavascriptExecutor) driver).executeScript(js, username);
+       //  String js2 = "arguments[0].setAttribute('value','"+pass+"')" ;
+      //   ((JavascriptExecutor) driver).executeScript(js2, password);
+        System.out.println("ooooooooooooooooooooooooooooo");
+           username.sendKeys("shtek@yahoo.com");
+         Thread.sleep(2000);
+
+
+
+          password.sendKeys("passwprd");
+         Thread.sleep(2000);
+          password.submit();
+//          WebElement submit=driver.findElementByXPath("//*[contains(@class,'button form-submit ripple-effect')]");
+
+  //       submit.click();
+        // System.out.println("xxxxxxx" + submit.getText());
+         //
+         //
+         Thread.sleep(30000);
+         System.out.println("vvvvvvvvvvvvvvvvv");
+
+         //System.out.println("bzz" + username.s);
          // response= driver.getPageSource();
      }
      catch (Throwable t){
